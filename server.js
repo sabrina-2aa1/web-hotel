981bfc32763d5fffd879e17d4375a8d3e75743ba
sabrina-2aa1 <2721553@collegelacite.ca>
2025-07-19 23:39:31 -0400

    Ajout des routes et des vues handlebars (home, chambres, contact, apropos)

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Configurer Handlebars
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route principale
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
+app.get('/chambres', (req, res) => {
+  res.render('chambres', { title: 'Nos chambres' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
+});
+
+app.get('/apropos', (req, res) => {
+  res.render('apropos', { title: 'À propos' });
+});
